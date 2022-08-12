a40aba33e56667848eacfb758a4254075fca0081
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-13 00:51:59 +0900

    test

--- js/main.js
+++ js/main.js
@@ -3,26 +3,32 @@
 window.addEventListener('load', () => {
     console.log('load');
 
+    addEvent()
+
     // document.getElementById('start').addEventListener('click', (function (){
     //     return function f() {
     //         console.log('click!!');
-
-            window.addEventListener('deviceorientation', e => {
-                const absolute = e.absolute
-                const alpha    = e.alpha
-                const beta     = e.beta
-                const gamma    = e.gamma
-        
-                // const outputText = `absolute:${absolute},alpha:${alpha},beta:${beta},gamma:${gamma}`
-        
-                document.getElementById('absolute').innerHTML = absolute
-                document.getElementById('alpha').innerHTML = alpha
-                document.getElementById('beta').innerHTML = beta
-                document.getElementById('gamma').innerHTML = gamma
-            })
-
+    //         addEvent()
     //         document.getElementById('start').removeEventListener('click', f)
     //     }
     // })())
 
 })
+
+const addEvent = () => {
+    window.addEventListener('deviceorientation', e => {
+        const absolute = e.absolute
+        const alpha    = e.alpha
+        const beta     = e.beta
+        const gamma    = e.gamma
+
+        // const outputText = `absolute:${absolute},alpha:${alpha},beta:${beta},gamma:${gamma}`
+
+        document.getElementById('absolute').innerHTML = absolute
+        document.getElementById('alpha').innerHTML = alpha
+        document.getElementById('beta').innerHTML = beta
+        document.getElementById('gamma').innerHTML = gamma
+
+        document.getElementById('arrow').style.transform = `rotate(${alpha}deg)`
+    })
+}
