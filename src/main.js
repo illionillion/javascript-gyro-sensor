a7f66f6cc09534d88decc26f16d0f4bc73ed7b95
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-13 10:11:29 +0900

    オレオレサーバーうまくいかなかった

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// import { https } from 'https'
+// import { fs } from 'fs'
+const https = require('https');
+const fs = require('fs');
+
+
+const sslServerKey = 'server-key.pem';
+const sslServerCrt = 'server-crt.pem';
+
+const options = {
+    key: fs.readFileSync(sslServerKey),
+    cert: fs.readFileSync(sslServerCrt)
+};
+
+const server = https.createServer(options,
+    (request, response) => {
+        fs.readFile('./index.html', 'UTF-8',
+        (error, data) => {
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.write(data);
+            response.end();
+        });
+    }
+);
+server.listen(3001);
+console.log('server running...')
